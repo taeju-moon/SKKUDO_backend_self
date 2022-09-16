81af25501177787ecf258dc77566e95f298ab815
taeju-moon <pp2lycee@naver.com>
2022-09-16 11:53:02 +0900

    a

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res, next)=>{
+app.get("/", (req:express.Request, res:express.Response, next:express.NextFunction)=>{
     res.send("hello");
 });
 
 app.listen(8000, ()=>{
     console.log("8000번 포트 대기중...")
-})
+});
