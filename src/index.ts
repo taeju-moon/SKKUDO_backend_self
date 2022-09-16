7b39029409c7133b7264fbef95b29fc57ec43042
taeju-moon <pp2lycee@naver.com>
2022-09-16 16:51:07 +0900

    get folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req:express.Request, res:express.Response, next:express.NextFunction)=>{
-    res.send("hello");
-});
+app.get(
+  '/',
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.send('hello');
+  }
+);
 
-app.listen(8000, ()=>{
-    console.log("8000번 포트 대기중...")
+app.listen(8000, () => {
+  console.log('8000번 포트 대기중...');
 });
