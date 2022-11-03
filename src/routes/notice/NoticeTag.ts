6b35204197e045eea90853474720d47c5465d94b
taeju-moon <pp2lycee@naver.com>
2022-11-03 19:10:06 +0900

    add notice tag

--- src/routes/notice/NoticeTag.ts
+++ src/routes/notice/NoticeTag.ts
@@ -2,18 +2,22 @@ import express from 'express';
 import {
   getAllNoticeTags,
   getOneNoticeTag,
+  getNoticeTagsByClubId,
   createNoticeTag,
   deleteNoticeTag,
 } from '../../controllers/notice/NoticeTag';
 import {
   authByValidationTable,
   auth,
+  authByClub,
   authBySuperUser,
 } from '../../middlewares/auth';
 
 const NoticeTagRouter = express.Router();
 
 NoticeTagRouter.get('/', auth, authBySuperUser, getAllNoticeTags);
+
+NoticeTagRouter.get('/:clubId', authByClub, getNoticeTagsByClubId);
 
 NoticeTagRouter.get('/:id', getOneNoticeTag);
 
