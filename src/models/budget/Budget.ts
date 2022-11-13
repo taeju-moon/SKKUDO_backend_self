f29cdd798b14493ffe42065f35533d3f17a07be7
taeju-moon <pp2lycee@naver.com>
2022-11-13 22:24:33 +0900

    a

--- src/models/budget/Budget.ts
+++ src/models/budget/Budget.ts
@@ -1,30 +1,32 @@
 import { Budget as BudgetInterface } from '../../types/budget';
 import { Schema, model } from 'mongoose';
 import { BudgetRow as BudgetRowInterface } from '../../types/budget';
-    
-const budgetRowSchema = new Schema<BudgetRowInterface>({
-    date: Date,
-    income: String,
-    expense: String,
-    whom: String,
-    content: String,
-    balance: String,
-    note: String,
-    account: String
-  })
 
-  const budgetSchema = new Schema<BudgetInterface>({
-    clubId: {
-        type: Schema.Types.ObjectId,
-        required: true,
+const budgetRowSchema = new Schema<BudgetRowInterface>({
+  date: Date,
+  income: String,
+  expense: String,
+  whom: String,
+  content: String,
+  balance: String,
+  note: String,
+  account: String,
+});
+
+const budgetSchema = new Schema<BudgetInterface>({
+  clubId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  name: { type: String, required: true },
+  rows: [
+    {
+      type: budgetRowSchema,
     },
-    name: String,
-    rows: [{
-            type: budgetRowSchema
-    }],
-    createdAt: Date,
-    updatedAt: Date
-  })
+  ],
+  createdAt: Date,
+  updatedAt: Date,
+});
 
 const Budget = model<BudgetInterface>('Budget', budgetSchema);
 export { budgetSchema, Budget };
