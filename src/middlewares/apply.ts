6c0fa42258a76fb23abeec32c71618910c7cd175
taeju-moon <pp2lycee@naver.com>
2022-11-15 20:45:54 +0900

    a

--- src/middlewares/apply.ts
+++ src/middlewares/apply.ts
@@ -7,7 +7,8 @@ export const isThereAppliedUsers: Middleware = (req, res, next) => {
       if (data.length !== 0)
         res.status(403).json({
           status: 'fail',
-          error: '이미 해당 Applier에 지원한 유저가 있습니다.',
+          error:
+            '이미 지원한 유저가 있어 지원 양식을 수정/삭제 할 수 없습니다. 지원자들을 모두 삭제하고 이용해주세요.',
         });
       else next();
     })
